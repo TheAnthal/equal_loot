22800650d99f95a08b4e42781840c869bc767abc
TheAnthal <ademerse@gmail.com>
2023-10-12 10:23:20 -0400

    LH brute forece fixes

--- src/mod.ts
+++ src/mod.ts
@@ -91,13 +91,52 @@ class Mod implements IPostDBLoadMod
         const intSpawns = tables.locations.interchange.looseLoot.spawnpoints;
         for (const spotBuffs of intSpawns)
         {
-            // Mall
+            // Interchange - Mall
             if ((spotBuffs.template.Position.x > -181) && (spotBuffs.template.Position.x < 95) && (spotBuffs.template.Position.y > 25) && (spotBuffs.template.Position.y < 39) && (spotBuffs.template.Position.z > -306) && (spotBuffs.template.Position.z < 196))
             {
                 spotBuffs.probability = 1;
             },
-            // Power
+            // Interchange - Power
             if ((spotBuffs.template.Position.x > -220) && (spotBuffs.template.Position.x < -190) && (spotBuffs.template.Position.y > 20) && (spotBuffs.template.Position.y < 24) && (spotBuffs.template.Position.z > -364) && (spotBuffs.template.Position.z < -341))
+            {
+                spotBuffs.probability = 1;
+            }
+        }
+        const lhSpawns = tables.locations.lighthouse.looseLoot.spawnpoints;
+        for (const spotBuffs of lhSpawns)
+        {
+            // Lighthouse - Red Chalet
+            if ((spotBuffs.template.Position.x > -155) && (spotBuffs.template.Position.x < -58) && (spotBuffs.template.Position.y > 26) && (spotBuffs.template.Position.y < 43) && (spotBuffs.template.Position.z > 65) && (spotBuffs.template.Position.z < 137))
+            {
+                spotBuffs.probability = 1;
+            },
+            // Lighthouse - Blue  Chalet
+            if ((spotBuffs.template.Position.x > -120) && (spotBuffs.template.Position.x < -68) && (spotBuffs.template.Position.y > 17) && (spotBuffs.template.Position.y < 23) && (spotBuffs.template.Position.z > -110) && (spotBuffs.template.Position.z < 13))
+            {
+                spotBuffs.probability = 1;
+            },
+            // Lighthouse - Construction
+            if ((spotBuffs.template.Position.x > 9) && (spotBuffs.template.Position.x < 28) && (spotBuffs.template.Position.y > 4) && (spotBuffs.template.Position.y < 6) && (spotBuffs.template.Position.z > -248) && (spotBuffs.template.Position.z < -110))
+            {
+                spotBuffs.probability = 1;
+            },
+            // Lighthouse - Village
+            if ((spotBuffs.template.Position.x > -282) && (spotBuffs.template.Position.x < -118) && (spotBuffs.template.Position.y > 9) && (spotBuffs.template.Position.y < 14) && (spotBuffs.template.Position.z > -256) && (spotBuffs.template.Position.z < -160))
+            {
+                spotBuffs.probability = 1;
+            },
+            // Lighthouse - Sunken
+            if ((spotBuffs.template.Position.x > -348) && (spotBuffs.template.Position.x < -219) && (spotBuffs.template.Position.y > -1) && (spotBuffs.template.Position.y < 4) && (spotBuffs.template.Position.z > -388) && (spotBuffs.template.Position.z < -314))
+            {
+                spotBuffs.probability = 1;
+            },
+            // Lighthouse - Water Treatment
+            if ((spotBuffs.template.Position.x > -201) && (spotBuffs.template.Position.x < 73) && (spotBuffs.template.Position.y > -1) && (spotBuffs.template.Position.y < 15) && (spotBuffs.template.Position.z > -760) && (spotBuffs.template.Position.z < -450))
+            {
+                spotBuffs.probability = 1;
+            },
+            // Lighthouse - Trainyard
+            if ((spotBuffs.template.Position.x > -141) && (spotBuffs.template.Position.x < 60) && (spotBuffs.template.Position.y > 9) && (spotBuffs.template.Position.y < 13) && (spotBuffs.template.Position.z > -956) && (spotBuffs.template.Position.z < -788))
             {
                 spotBuffs.probability = 1;
             }
